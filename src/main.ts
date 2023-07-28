2fa2c91f0c94bca11c6ed99a10de69df56c2439f
Tom Zidani <tom.zidani@gmail.com>
2023-07-28 19:51:20 +0200

    Update assets pos.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,1 @@
 import "./styles/main.scss"
-// import Benefits from "./components/Benefits"
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const benefits = document.querySelector("[data-component='benefits']")!
-//   new Benefits(benefits)
-// })
